9e80e53032197297fcbfd96982e5384483745c8d
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-05 16:31:25 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 //  Khansa Tahu Tuna Pacitan ©2023
  	// membuat elemen paragraf
-		var paragraph = document.createElement('p');
+		let paragraph = document.createElement('p');
 		paragraph.textContent = 'Ini adalah contoh sederhana untuk membuat web semantic dengan JavaScript DOM.';
